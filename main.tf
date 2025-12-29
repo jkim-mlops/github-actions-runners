c4a2d421faa77c9a0ed153d0a9e7bb0897c11cd6
jkim-mlops <josephykim317@gmail.com>
2025-12-28 22:41:09 -0500

    refactor(main.tf): :arrow_up: upgrade to 0.2.0 for external modules

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
 */
 
 module "vpc" {
-  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/vpc?ref=0.1.0"
+  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/vpc?ref=0.2.0"
 
   name       = var.name
   region     = var.region
@@ -26,7 +26,7 @@ module "runner" {
 }
 
 module "ecs" {
-  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/ecs?ref=feat/fargate-cp"
+  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/ecs?ref=0.2.0"
 
   name               = var.name
   cidr_blocks        = [module.vpc.vpc_cidr_block]
@@ -99,7 +99,7 @@ module "ecs" {
 }
 
 module "webhook" {
-  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/docker?ref=0.1.2"
+  source = "git@github.com:jkim-mlops/terraform-modules.git//modules/docker?ref=0.2.0"
 
   image_name    = "${var.name}-webhook"
   image_tag     = var.webhook_image_tag
